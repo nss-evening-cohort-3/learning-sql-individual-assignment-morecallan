cc804dae7285fecaab9320e65d8edb3011175453
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-27 12:56:43 -0500

    insert instructions for 01 and 02

--- 1.sql
+++ 1.sql
@@ -1,2 +1,5 @@
+/*Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.*/
+
+
 SELECT FirstName || ' ' || LastName AS 'Full Name', CustomerId, Country FROM Customer
 WHERE Country != 'US';
