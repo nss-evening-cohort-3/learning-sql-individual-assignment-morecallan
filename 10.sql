210cf1deb4de295a4096441ced22d3e36b6a3ea8
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-28 11:05:11 -0500

    11 complete

--- 10.sql
+++ 10.sql
@@ -2,3 +2,8 @@
 
 SELECT COUNT(InvoiceLineId) From InvoiceLine
 WHERE InvoiceId == 37;
+
+/*Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY*/
+
+SELECT COUNT(InvoiceLineId), InvoiceId From InvoiceLine
+GROUP BY InvoiceId;
