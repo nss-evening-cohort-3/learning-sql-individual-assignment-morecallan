c4bc0dfa7210c5cd5186060b101316e3f387a035
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-28 10:30:29 -0500

    09 complete

--- 7.sql
+++ 7.sql
@@ -4,3 +4,7 @@
 SELECT Employee.FirstName || ' ' || Employee.LastName AS 'Support Rep', InvoiceId, Invoice.CustomerId, InvoiceDate, BillingAddress, BillingCity, BillingState, BillingCountry, BillingPostalCode, Total FROM Invoice
 JOIN Customer ON Invoice.CustomerId == Customer.CustomerId
 JOIN Employee ON Employee.EmployeeId == Customer.SupportRepId;
+
+SELECT Employee.FirstName || ' ' || Employee.LastName AS 'Support Rep', Invoice.*FROM Invoice
+JOIN Customer ON Invoice.CustomerId == Customer.CustomerId
+JOIN Employee ON Employee.EmployeeId == Customer.SupportRepId;
---
+++ 9.sql
@@ -0,0 +1,9 @@
+/* How many Invoices were there in 2009 and 2011? */
+
+SELECT COUNT(InvoiceDate) FROM Invoice
+WHERE InvoiceDate LIKE '%2009%' OR InvoiceDate LIKE '%2011%'
+
+/* What are the respective total sales for each of those years? */
+
+SELECT TOTAL(Total) FROM Invoice
+WHERE InvoiceDate LIKE '%2009%' OR InvoiceDate LIKE '%2011%'
