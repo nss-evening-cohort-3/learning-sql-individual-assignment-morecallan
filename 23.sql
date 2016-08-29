432806ee5168b0db05ebb33ef1642b8724cc3026
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-28 20:03:43 -0500

    23b complete - notes about attempts that worked and those that did not.

--- 23.sql
+++ 23.sql
@@ -9,3 +9,15 @@ SELECT TOTAL(Invoice.Total) AS 'Total Spent Per Country', Invoice.BillingCountry
 GROUP BY Invoice.BillingCountry
 ORDER BY 1 DESC
 LIMIT 1;
+
+/* **** WORKED **** */
+SELECT TOTAL(Invoice.Total) AS 'Total Spent Per Country', Invoice.BillingCountry FROM Invoice
+GROUP BY Invoice.BillingCountry
+ORDER BY "Total Spent Per Country" DESC
+LIMIT 1
+
+/* **** DID NOT WORK WHAT THE HELL **** */
+SELECT TOTAL(Invoice.Total) AS 'Total Spent Per Country', Invoice.BillingCountry FROM Invoice
+GROUP BY Invoice.BillingCountry
+ORDER BY 'Total Spent Per Country' DESC
+LIMIT 1
