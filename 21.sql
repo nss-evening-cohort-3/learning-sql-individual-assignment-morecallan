68b35e22441f3e315316c71dbfe1c1b567a25fe6
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-29 20:03:05 -0500

    attempt at using MAX. hate it. over it.

--- 21.sql
+++ 21.sql
@@ -1,1 +1,8 @@
 /*Which sales agent made the most in sales over all?*/
+
+SELECT TOTAL(Invoice.Total) AS 'Sales Total', Employee.FirstName || ' ' || Employee.LastName AS 'Sales Rep' FROM Invoice
+JOIN Customer ON Invoice.CustomerId == Customer.CustomerId
+JOIN Employee ON Customer.SupportRepId == Employee.EmployeeId
+GROUP BY Employee.EmployeeId
+ORDER BY "Sales Total" DESC
+LIMIT 1;
